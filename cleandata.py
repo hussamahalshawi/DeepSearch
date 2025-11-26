feaee5e6499b76833ce1b19cc6d49cb33f3156d7
Hussam Alshawi <hussamahalshawi@gmail.com>
2025-11-26 21:25:54 +0200

    sorted_by_output_search and print the out put

--- cleandata.py
+++ cleandata.py
@@ -22,17 +22,13 @@ class CleanData:
         for key, value in self.data.items():
             self.data_all_new[key] = []
             for word in value:
-                # print("-+-+/", word)
                 if type(word) == int or type(word) == float:
                     value.remove(word)
                 else:
                     word = str(word)
-                    # print("*****/", word)
                     word = re.findall(pattern, word)
                     if word != []:
                         word_new = ''.join(map(str, word))
-                        # print(word_new)
-                        # print("//////////////////")
                         self.data_all_new[key].append(word_new)
             self.paragraph[key] = ''.join(map(str, value))
         return self.paragraph, self.data_all_new
